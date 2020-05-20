64a2356e228357e0c2cf610e8c96f5958f63294c
2rockkk <65488254+2rockkk@users.noreply.github.com>
2020-05-20 17:27:57 +0300

    Add files via upload

---
+++ Сапер v2.1.2/saper/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace saper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sizeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x10ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.x5ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contactsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.attemptsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackgroundImage = global::saper.Properties.Resources._31444144_idilliaco_seamless_grass_texture;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.gameToolStripMenuItem,
+            this.contactsToolStripMenuItem,
+            this.attemptsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(401, 30);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            this.menuStrip1.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.MenuStrip1_ItemClicked);
+            // 
+            // gameToolStripMenuItem
+            // 
+            this.gameToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sizeToolStripMenuItem});
+            this.gameToolStripMenuItem.Name = "gameToolStripMenuItem";
+            this.gameToolStripMenuItem.Size = new System.Drawing.Size(62, 26);
+            this.gameToolStripMenuItem.Text = "Game";
+            this.gameToolStripMenuItem.Click += new System.EventHandler(this.GameToolStripMenuItem_Click);
+            // 
+            // sizeToolStripMenuItem
+            // 
+            this.sizeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.x10ToolStripMenuItem,
+            this.x5ToolStripMenuItem});
+            this.sizeToolStripMenuItem.Name = "sizeToolStripMenuItem";
+            this.sizeToolStripMenuItem.Size = new System.Drawing.Size(119, 26);
+            this.sizeToolStripMenuItem.Text = "Size";
+            this.sizeToolStripMenuItem.Click += new System.EventHandler(this.SizeToolStripMenuItem_Click);
+            // 
+            // x10ToolStripMenuItem
+            // 
+            this.x10ToolStripMenuItem.Name = "x10ToolStripMenuItem";
+            this.x10ToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.x10ToolStripMenuItem.Text = "10x10";
+            this.x10ToolStripMenuItem.Click += new System.EventHandler(this.X10ToolStripMenuItem_Click);
+            // 
+            // x5ToolStripMenuItem
+            // 
+            this.x5ToolStripMenuItem.Name = "x5ToolStripMenuItem";
+            this.x5ToolStripMenuItem.Size = new System.Drawing.Size(131, 26);
+            this.x5ToolStripMenuItem.Text = "5x5";
+            this.x5ToolStripMenuItem.Click += new System.EventHandler(this.X5ToolStripMenuItem_Click);
+            // 
+            // contactsToolStripMenuItem
+            // 
+            this.contactsToolStripMenuItem.Name = "contactsToolStripMenuItem";
+            this.contactsToolStripMenuItem.Size = new System.Drawing.Size(80, 26);
+            this.contactsToolStripMenuItem.Text = "Contacts";
+            this.contactsToolStripMenuItem.Click += new System.EventHandler(this.ContactsToolStripMenuItem_Click);
+            // 
+            // attemptsToolStripMenuItem
+            // 
+            this.attemptsToolStripMenuItem.Name = "attemptsToolStripMenuItem";
+            this.attemptsToolStripMenuItem.Size = new System.Drawing.Size(84, 26);
+            this.attemptsToolStripMenuItem.Text = "Attempts";
+            this.attemptsToolStripMenuItem.Click += new System.EventHandler(this.AttemptsToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::saper.Properties.Resources.unnamed;
+            this.ClientSize = new System.Drawing.Size(401, 406);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "sapper";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x10ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem x5ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem contactsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem attemptsToolStripMenuItem;
+    }
+}
+
---
+++ Сапер v2.1.2/saper/Form1.cs
@@ -0,0 +1,352 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Text;
+using System.Windows.Forms;
+
+namespace saper
+{
+    public partial class Form1 : Form
+    {
+        int width = 10;
+        int height = 10;
+        int offset = 30;
+        int bombPercent = 15;
+        bool isFirstCLick = true;
+        FieldButton[,] field;
+        int cellsOpened = 0;
+        int bombs = 0;
+        string path1 = @"c:/Сапер v2.1.2/wins.txt";
+        string path2 = @"c:/Сапер v2.1.2/loses.txt";
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GenerateField()
+        {
+            Random random = new Random();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    FieldButton newButton = new FieldButton();
+                    newButton.Location = new Point(x * offset, (y + 1) * offset);
+                    newButton.Size = new Size(offset, offset);
+                    newButton.isClickable = true;
+                    newButton.TabStop = false;
+
+
+
+                    if (random.Next(0, 100) <= bombPercent)
+                    {
+                        newButton.isBomb = true;
+                        bombs++;
+                    }
+                    newButton.xCoord = x;
+                    newButton.yCoord = y;
+
+                    Controls.Add(newButton);
+                    newButton.MouseUp += new MouseEventHandler(FieldButtonClick);
+                    field[x, y] = newButton;
+                }
+            }
+        }
+
+        void FieldButtonClick(object sender, MouseEventArgs e)
+        {
+
+            FieldButton clickedButton = (FieldButton)sender;
+            if (e.Button == MouseButtons.Left && clickedButton.isClickable)
+            {
+                clickedButton.BackColor = Color.LightSlateGray;
+                if (clickedButton.isBomb)
+                {
+                    if (isFirstCLick)
+                    {
+                        clickedButton.isBomb = false;
+                        isFirstCLick = false;
+                        bombs--;
+                        OpenRegion(clickedButton.xCoord, clickedButton.yCoord, clickedButton);
+                    }
+                    else
+                    {
+                        Explode();
+                    }
+
+                }
+                else
+                {
+                    EmptyFieldButtonClick(clickedButton);
+                }
+                isFirstCLick = false;
+            }
+            if (e.Button == MouseButtons.Right)
+            {
+
+                clickedButton.isClickable = !clickedButton.isClickable;
+                if (!clickedButton.isClickable)
+                {
+                    playFlag();
+                    clickedButton.Text = "B";
+                }
+                else
+                {
+                    playFlag();
+                    clickedButton.Text = "";
+
+                }
+            }
+            CheckWin();
+        }
+
+        void Explode()
+        {
+
+            foreach (FieldButton button in field)
+            {
+                if (button.isBomb)
+                {
+                    button.Text = "*";
+                }
+            }
+            playExplode();
+            MessageBox.Show("¯/_(ツ)_/¯ u lose...");
+            CountLoses();
+            Application.Restart();
+        }
+        void EmptyFieldButtonClick(FieldButton clickedButton)
+        {
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (field[x, y] == clickedButton)
+                    {
+
+                        OpenRegion(x, y, clickedButton);
+                    }
+                }
+            }
+
+
+        }
+        void OpenRegion(int xCoord, int yCoord, FieldButton clickedButton)
+        {
+            Queue<FieldButton> queue = new Queue<FieldButton>();
+            queue.Enqueue(clickedButton);
+            clickedButton.wasAdded = true;
+            while (queue.Count > 0)
+            {
+                FieldButton currentCell = queue.Dequeue();
+                OpenCell(currentCell.xCoord, currentCell.yCoord, currentCell);
+                cellsOpened++;
+                if (CountBombsAround(currentCell.xCoord, currentCell.yCoord) == 0)
+                {
+                    for (int y = currentCell.yCoord - 1; y <= currentCell.yCoord + 1; y++)
+                    {
+                        for (int x = currentCell.xCoord - 1; x <= currentCell.xCoord + 1; x++)
+                        {
+                            if (x == currentCell.xCoord && y == currentCell.yCoord)
+                            {
+                                continue;
+                            }
+                            if (x >= 0 && x < width && y >= 0 && y < height)
+                            {
+                                if (!field[x, y].wasAdded)
+                                {
+                                    queue.Enqueue(field[x, y]);
+                                    field[x, y].wasAdded = true;
+                                    field[x, y].BackColor = Color.LightSlateGray;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+        }
+        void OpenCell(int x, int y, FieldButton clickedButton)
+        {
+            int bombsAround = CountBombsAround(x, y);
+            if (bombsAround == 0)
+            {
+
+            }
+            else
+            {
+                clickedButton.Text = "" + bombsAround;
+            }
+            clickedButton.Enabled = false;
+        }
+
+        int CountBombsAround(int xCoord, int yCoord)
+        {
+            int bombsAround = 0;
+            for (int x = xCoord - 1; x <= xCoord + 1; x++)
+            {
+                for (int y = yCoord - 1; y <= yCoord + 1; y++)
+                {
+                    if (x >= 0 && x < width && y >= 0 && y < height)
+                    {
+                        if (field[x, y].isBomb == true)
+                        {
+                            bombsAround++;
+                        }
+                    }
+                }
+            }
+            return bombsAround;
+        }
+        void CheckWin()
+        {
+            int cells = width * height;
+            int emptyCells = cells - bombs;
+            if (cellsOpened == emptyCells)
+            {
+                CountWins();
+                playWin();
+                MessageBox.Show("(╯°□°）╯︵ ┻━┻ u win! ");
+                Application.Restart();
+
+            }
+
+
+
+        }
+        void CountWins()
+        {
+            StreamReader sr = new StreamReader(path1);
+            int i = Convert.ToInt32(sr.ReadToEnd());
+            sr.Close();
+            int x = i + 1;
+            StreamWriter sr1 = new StreamWriter(path1, false, Encoding.Default);
+            sr1.WriteLine(x);
+            sr1.Close();
+        }
+        void CountLoses()
+        {
+            StreamReader sr = new StreamReader(path2);
+            int i = Convert.ToInt32(sr.ReadToEnd());
+            sr.Close();
+            int x = i + 1;
+            StreamWriter sr1 = new StreamWriter(path1, false, Encoding.Default);
+            sr1.WriteLine(x);
+            sr1.Close();
+        }
+
+        private void X10ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playMenu();
+            width = 10;
+            height = 10;
+            field = new FieldButton[width, height];
+            GenerateField();
+            menuStrip1.Enabled = false;
+
+        }
+
+        private void X5ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playMenu();
+            width = 5;
+            height = 5;
+            field = new FieldButton[width, height];
+            GenerateField();
+            menuStrip1.Enabled = false;
+            this.Size = new System.Drawing.Size(167, 220);
+
+
+
+        }
+        private void playMenu()
+        {
+            SoundPlayer menuClick = new SoundPlayer(@"c:\Сапер v2.1\door_open.wav");
+            menuClick.Play();
+        }
+        private void playExplode()
+        {
+            SoundPlayer menuClick = new SoundPlayer(@"c:\Сапер v2.1\explode1.wav");
+            menuClick.Play();
+        }
+        private void playWin()
+        {
+            SoundPlayer menuClick = new SoundPlayer(@"c:\Сапер v2.1\successful_hit.wav");
+            menuClick.Play();
+        }
+        private void playFlag()
+        {
+            SoundPlayer menuClick = new SoundPlayer(@"c:\Сапер v2.1\pop.wav");
+            menuClick.Play();
+        }
+
+
+        private void LevelToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ToolStripMenuItem3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playMenu();
+        }
+
+        private void SizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playMenu();
+        }
+
+        private void MenuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void ContactsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playMenu();
+            MessageBox.Show("Turok_SV_18@mf.grsu.by");
+
+        }
+
+        private void AttemptsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            playMenu();
+            StreamReader sr = new StreamReader(path1);
+            string s = sr.ReadToEnd();
+            sr.Close();
+            StreamReader sg = new StreamReader(path2);
+            string g = sg.ReadToEnd();
+            sg.Close();
+            MessageBox.Show("Общее количество побед: " + s +"." + "Общее количество поравжений:"+ g );
+
+        }
+    }
+
+
+
+    public class FieldButton : Button
+    {
+        public bool isBomb;
+        public bool isClickable;
+        public bool wasAdded;
+        public int xCoord;
+        public int yCoord;
+    }
+}
+
